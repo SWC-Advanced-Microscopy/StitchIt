4f0ff0281f40fd63d290e994376cc23c05039cbd
Fouga <natalia.chicherova@gmail.com>
2017-06-22 15:10:22 +0200

    added comments to cidreIllumcorrection.m function

--- code/processDuringAcquistion/cidreIllumcorrection.m
+++ code/processDuringAcquistion/cidreIllumcorrection.m
@@ -1,10 +1,14 @@
 function cidreIllumcorrection
-% default post acquisition function. 
+% The function calculates background images to correct nonlinear
+% illumination for Tissue Vision tiles.
+% One model is calculated per 1 channel.
+% The model.mat files are save in a rawData/avarageDir
+% The following stitching then checks if this mat file exist or not and
+% corrects the background if the file exist.
 %
-% You may write your own and have it run via the INI file. 
-%
-%
-% Rob Campbell - Basel 2015
+% stitchitConf.ini file needs to have doIlluminationCorrection = 1
+% 
+% Natalia Chicherova - Basel, 2017
 
 
 % destination folder is average dir of the average images
@@ -14,12 +18,13 @@ if ~exist(avDir,'dir')
     mkdir(avDir)
 end
 
+% find the number of channels for stitching
 chansToStitch=channelsAvailableForStitching;
 
 % calculate background model for illumination correction with CIDRE
 for thisChan=chansToStitch 
     tic;
-     % source directory name is adjusted to the Tissue way of saving
+     % source directory name is adjusted to the tissue cyte way of saving
     source = sprintf('./rawData/*_0%i.tif',thisChan);
     cidre(source, 'destination',avDir);
     t=toc/60;
