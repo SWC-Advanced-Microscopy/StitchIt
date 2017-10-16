4738e648d7db7987d4f60dd18c4d0ef2638dda78
Rob Campbell <git@raacampbell.com>
2017-10-16 12:14:39 +0200

    Allow stitching to proceed even if channels are missing in some sections.

--- code/SystemClasses/@bakingtray/tileLoad.m
+++ code/SystemClasses/@bakingtray/tileLoad.m
@@ -86,9 +86,8 @@ sectionNum = coords(1);
 planeNum = coords(2); %Optical plane
 channel = coords(5);
 
-%TODO: we're just loading the full stack right now
 im=[];
-
+index=[];
 
 %Check that all requested data exist
 for XYposInd=1:size(positionArray,1)
@@ -96,8 +95,6 @@ for XYposInd=1:size(positionArray,1)
     path2stack = fullfile(sectionDir,sectionTiff);
     if ~exist(path2stack,'file') %TODO: bad [why? -- RAAC 02/05/2017]
         fprintf('%s - Can not find stack %s. RETURNING EMPTY DATA. BAD.\n', mfilename, path2stack);
-        im=[];
-        index=[];
         positionArray=[];
         return
     end
