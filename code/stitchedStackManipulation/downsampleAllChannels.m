1d669b954d6595bfdcae0a4444d1e266e4c38201
Rob Campbell <git@raacampbell.com>
2019-06-05 09:47:39 +0100

    downsampled images TIFF by default

--- code/stitchedStackManipulation/downsampleAllChannels.m
+++ code/stitchedStackManipulation/downsampleAllChannels.m
@@ -1,14 +1,15 @@
-function downsampleAllChannels(voxelSize)
-% function downsampleAllChannels(voxelSize)
+function downsampleAllChannels(voxelSize,fileFormat)
+% function downsampleAllChannels(voxelSize,fileFormat)
 %
 % Purpose
-% Downsample all channels to MHD files with a voxel size defined by
+% Downsample all channels to MHD or TIFF files with a voxel size defined by
 % "voxelSize". If voxelSize is missing, we use 25 microns. Then copy
-% all to a single directory.
+% all to a single directory. 
 %
 % Inputs
 % voxelSize - a scalar (25 by default) defining the target voxel
 %             size of the resample operation.
+% fileFormat - 'MHD' or 'TIFF'. TIFF by default
 %
 %
 % Rob Campbell - SWC, 2018
@@ -20,25 +21,30 @@ function downsampleAllChannels(voxelSize)
     return
   end
 
-  if nargin<1
+  if nargin<1 || isempty(voxelSize)
     voxelSize=25;
   end
+  
+  if nargin<2 || isempty(fileFormat)
+      fileFormat='tiff';
+  end
 
-  dsDirName=sprintf('downsampledMHD_%d', round(voxelSize));
+  dsDirName=sprintf('downsampledStacks_%d', round(voxelSize));
 
 
   if ~exist(dsDirName,'dir')
     mkdir(dsDirName)
   end
 
-
+  % Which channels are available?
   chan = stitchedDataInfo.channelsPresent;
 
+  % Downsample those channels
   fnames={};
   for ii = 1:length(chan)
     tChan = chan(ii);
     fprintf('Making downsampled volume for channel %d\n', tChan)
-    [~,tFname]=resampleVolume(tChan,voxelSize);
+    [~,tFname]=resampleVolume(tChan,voxelSize,fileFormat); %Downsample
     fnames{ii} = tFname;
   end
 
