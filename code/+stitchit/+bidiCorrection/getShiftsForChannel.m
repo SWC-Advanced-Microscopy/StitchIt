9e5e6ed35fdac076301bc29e5d9ee15113d3f854
Rob Campbell <git@raacampbell.com>
2021-01-21 12:17:02 +0000

    bugfix to section batcher for bidi

--- code/+stitchit/+bidiCorrection/getShiftsForChannel.m
+++ code/+stitchit/+bidiCorrection/getShiftsForChannel.m
@@ -5,11 +5,14 @@ function stats=getShiftForChannel(chan)
     section=handleSectionArg([]);
     section = section(section(:,2)==1,:);
 
-
     parfor ii=1:length(section)
-        tmp=stitchit.bidiCorrection.getShiftForSection(section(ii,1),chan);
+        tSection = section(ii,1);
+        fprintf('Starting section %d\n',tSection)
+        tmp=stitchit.bidiCorrection.getShiftForSection(tSection,chan);
+        fprintf('Done section %d\n',tSection)
+
         if ~isempty(tmp)
-            stats(ii)=tmp;
+            stats{ii}=tmp;
         end
     end
 
